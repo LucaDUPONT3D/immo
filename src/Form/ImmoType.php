af6684e58ec2fe26c401c5b722402a9695b1a30c
Luca DUPONT <luca.dupont@orange.fr>
2023-02-24 11:06:39 +0100

    Yolo2

--- src/Form/ImmoType.php
+++ src/Form/ImmoType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Immo;
+use Doctrine\DBAL\Types\DecimalType;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,17 @@ class ImmoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('area')
-            ->add('habitableRooms')
-            ->add('type')
+            ->add('name',TextType::class)
+            ->add('picture',TextType::class)
+            ->add('area', DecimalType::class)
+            ->add('habitableRooms',NumberType::class)
+            ->add(
+                'type',
+                ChoiceType::class,
+                ['choices' => ['Maison' => 'Maison', 'Appartement' => 'Appartement', 'Tente' => 'Tente', 'Yourte' => 'Yourte'],
+                'expanded' => false,
+                    'multiple' => false]
+            )
             ->add('adress')
             ->add('swimmingPool')
             ->add('outside')
